365ae2f72ebf3bedc134eca2fa8aa9267a28cc6f
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-24 19:05:02 +0200

    Add more test cases

--- src/domain/timeInWords.test.ts
+++ src/domain/timeInWords.test.ts
@@ -1,11 +1,38 @@
-import {timeInWords} from "./timeInWords";
+import {timeInWords, typedItem} from "./timeInWords";
 
-test('timeInWords works for all required cases', () => {
-    expect(timeInWords(new Date("2019-04-24T13:36:02")))
-        .toEqual([
+it.each([
+    [
+        "2019-04-24T19:03:34",
+        [
+            {"title": "fünf", "type": "time"},
+            {"title": "nach", "type": "time"},
+            {"title": "sieben", "type": "hour"},
+        ]
+    ],
+    [
+        "2019-04-24T13:36:02",
+        [
             {"title": "fünf", "type": "time"},
             {"title": "nach", "type": "time"},
             {"title": "halb", "type": "time"},
             {"title": "zwei", "type": "hour"},
-        ]);
+        ]
+    ],
+    [
+        "2019-04-24T18:57:04",
+        [
+            {"title": "fünf", "type": "time"},
+            {"title": "vor", "type": "time"},
+            {"title": "sieben", "type": "hour"},
+        ]
+    ],
+    [
+        "2019-04-24T18:59:50",
+        [
+            {"title": "um", "type": "time"},
+            {"title": "sieben", "type": "hour"},
+        ]
+    ],
+])('works for all cases', (date: string, expected: typedItem[]) => {
+    expect(timeInWords(new Date(date))).toEqual(expected);
 });
