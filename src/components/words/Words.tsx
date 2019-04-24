64c4af1030a69b11e64c9e6f90a137f0d6ab5e29
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-24 18:37:18 +0200

    Clean up component

--- src/components/words/Words.tsx
+++ src/components/words/Words.tsx
@@ -11,20 +11,23 @@ const WordInactive = styled.span`
 const WordsStyled = styled.div`
     text-transform: uppercase;
     font-size: 2em;
+    line-height: 1.35;
     width: 20vw;
+    text-align: center;
 `;
 
 type WordsProps = {
     date: Date;
 }
 
-export const Words: FunctionComponent<WordsProps> = React.memo((props) => {console.log(props); return <WordsStyled>
+export const Words: FunctionComponent<WordsProps> = React.memo(({date}) => <WordsStyled>
+    <WordActive>Es ist</WordActive>
     {
-        [...timeItems, ...hourItems].map(item => timeInWords(props.date).includes(item)
+        [...timeItems, ...hourItems].map(item => timeInWords(date).includes(item)
             ? <WordActive key={item.type + "-" + item.title}> {item.title} </WordActive>
             : <WordInactive key={item.type + "-" + item.title}> {item.title} </WordInactive>
         )
     }
-</WordsStyled>;}, (prev, next) => prev.date.getMinutes() === next.date.getMinutes());
+</WordsStyled>, (prev, next) => prev.date.getMinutes() === next.date.getMinutes());
 
 export default Words;
