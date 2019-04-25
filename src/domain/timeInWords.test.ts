b4fe2b970dbab11657c81ff01406e3cbd40c8ac0
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-25 10:33:02 +0200

    Add more test cases

--- src/domain/timeInWords.test.ts
+++ src/domain/timeInWords.test.ts
@@ -2,37 +2,45 @@ import {timeInWords, typedItem} from "./timeInWords";
 
 it.each([
     [
+        "2019-04-25T10:02:09",
+        [{"title": "um", "type": "time"}]
+    ],
+    [
         "2019-04-24T19:03:34",
-        [
-            {"title": "fünf", "type": "time"},
-            {"title": "nach", "type": "time"},
-            {"title": "sieben", "type": "hour"},
-        ]
+        [{"title": "fünf", "type": "time"}, {"title": "nach", "type": "time"}]
+    ],
+    [
+        "2019-04-25T10:12:45",
+        [{"title": "zehn", "type": "time"}, {"title": "nach", "type": "time"}]
+    ],
+    [
+        "2019-04-24T22:16:27",
+        [{"title": "viertel", "type": "time"}]
+    ],
+    [
+        "2019-04-24T19:19:37",
+        [{"title": "zwanzig", "type": "time"}, {"title": "nach", "type": "time"}]
+    ],
+    [
+        "2019-04-24T22:25:14",
+        [{"title": "fünf", "type": "time"}, {"title": "vor", "type": "time"}, {"title": "halb", "type": "time"}]
+    ],
+    [
+        "2019-04-25T10:30:55",
+        [{"title": "halb", "type": "time"}]
     ],
     [
         "2019-04-24T13:36:02",
-        [
-            {"title": "fünf", "type": "time"},
-            {"title": "nach", "type": "time"},
-            {"title": "halb", "type": "time"},
-            {"title": "zwei", "type": "hour"},
-        ]
+        [{"title": "fünf", "type": "time"}, {"title": "nach", "type": "time"}, {"title": "halb", "type": "time"}]
     ],
     [
         "2019-04-24T18:57:04",
-        [
-            {"title": "fünf", "type": "time"},
-            {"title": "vor", "type": "time"},
-            {"title": "sieben", "type": "hour"},
-        ]
+        [{"title": "fünf", "type": "time"}, {"title": "vor", "type": "time"}]
     ],
     [
         "2019-04-24T18:59:50",
-        [
-            {"title": "um", "type": "time"},
-            {"title": "sieben", "type": "hour"},
-        ]
+        [{"title": "um", "type": "time"}]
     ],
-])('works for all cases', (date: string, expected: typedItem[]) => {
-    expect(timeInWords(new Date(date))).toEqual(expected);
+])('works for all minutes', (date: string, expected: typedItem[]) => {
+    expect(timeInWords(new Date(date))).toEqual(expect.arrayContaining(expected));
 });
