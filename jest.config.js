c752adc60726238edfda8393b7fc8e4d2b1f67d6
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-25 10:34:02 +0200

    Change configs to get coverage running

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,10 @@ module.exports = {
     "transform": {
         "^.+\\.tsx?$": "ts-jest"
     },
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "**/*.{js,jsx,ts,tsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ]
 };
