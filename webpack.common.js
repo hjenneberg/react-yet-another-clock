e77193781558e6a4a98d6adaa02d4f3829088ff3
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-26 22:53:32 +0200

    Lint webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,35 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires,import/no-extraneous-dependencies */
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/main.tsx'
+        app: './src/main.tsx',
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
-                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-            }
-        ]
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.jsx', '.js' ]
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Yet Another Clock',
-            template: './dist/index.html'
+            template: './dist/index.html',
         }),
     ],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
