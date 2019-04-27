23ff610778ceec0622030ec6bf8397ed4253fc28
Jens Henneberg <hens.jenneberg@gmail.com>
2019-04-27 12:31:41 +0200

    Remove obsolete stories

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,23 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import Words, { Active, Inactive, WordsStyled } from '../src/components/words/Words';
-import App from '../src/components/App';
 
-storiesOf('a word', module)
-    .add('that is active', (): JSX.Element => (
-        <Active>Active</Active>
-    ))
-    .add('that is inactive', (): JSX.Element => (
-        <Inactive>Inactive</Inactive>
-    ));
+import Words, { WordActive, WordInactive, WordsStyled } from '../src/components/words/Words';
+import App from '../src/components/App';
 
 storiesOf('a styled word', module)
     .add('that is active', (): JSX.Element => (
-        <WordsStyled><Active>Active</Active></WordsStyled>
+        <WordsStyled><WordActive>Active</WordActive></WordsStyled>
     ))
     .add('that is inactive', (): JSX.Element => (
-        <WordsStyled><Inactive>Inactive</Inactive></WordsStyled>
+        <WordsStyled><WordInactive>Inactive</WordInactive></WordsStyled>
     ));
 
 storiesOf('certains words', module)
