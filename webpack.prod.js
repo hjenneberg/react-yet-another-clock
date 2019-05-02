2ae5fbaae70e8fbd6d95dd104d50a4360f7d33ed
Jens Henneberg <hens.jenneberg@gmail.com>
2019-05-02 14:58:48 +0200

    Extend configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,15 @@
-const ManifestPlugin = require("webpack-manifest-plugin");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+/* eslint-disable @typescript-eslint/no-var-requires,import/no-extraneous-dependencies */
+const ManifestPlugin = require('webpack-manifest-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
-    mode: "production",
-    devtool: "source-map",
+    mode: 'production',
+    devtool: 'source-map',
     plugins: [
         new ManifestPlugin(),
         // new BundleAnalyzerPlugin(),
-    ]
+    ],
 });
