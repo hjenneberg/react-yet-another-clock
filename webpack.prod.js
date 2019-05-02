47e175fb994f955baeb78fa4f117c55e601dcf41
Jens Henneberg <hens.jenneberg@gmail.com>
2019-05-02 21:58:44 +0200

    Change library target to UMD
    So that we have exports in our lib

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires,import/no-extraneous-dependencies */
 const path = require('path');
-const ManifestPlugin = require('webpack-manifest-plugin');
+// const ManifestPlugin = require('webpack-manifest-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 
 module.exports = merge(common, {
-    entry: {
-        app: './src/components/App.tsx',
-    },
+    entry: './src/components/App.tsx',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'lib'),
         library: 'react-yet-another-clock',
-        libraryTarget: 'commonjs2',
+        libraryTarget: 'umd',
     },
     mode: 'production',
-    devtool: 'source-map',
+    // devtool: 'source-map',
     externals: ['react'],
     plugins: [
-        new ManifestPlugin(),
+        // new ManifestPlugin(),
         // new BundleAnalyzerPlugin(),
     ],
 });
